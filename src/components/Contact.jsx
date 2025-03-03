83e1524552828d25dccff02b00cb867d20ae6c08
Erik Hansen <erhansen@civiconnect.ca>
2025-03-03 11:10:48 -0500

    contact form styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,19 +4,39 @@ import styles from "../styles/components/Contact.module.css";
 export default function Contact() {
 	return (
 		<div className={styles["contact-container"]}>
-			<h2 className={styles["contact-title"]}>Contact Me</h2>
-			<div className={styles["form-container"]}>
-				<form className={styles["contact-form"]}>
-					<div className={styles["form-group"]}>
-						<input type="text" placeholder="Name" />
-						<input type="email" placeholder="Email" />
+			<div className={styles["contact-content"]}>
+				<div className={styles["contact-info"]}>
+					<h2>Get in Touch</h2>
+					<div className={styles["contact-links"]}>
+						<a
+							href="mailto:Ejhansen94@gmail.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Email
+						</a>
+						<a
+							href="https://www.linkedin.com/in/erik-hansen-a70180206
+"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							LinkedIn
+						</a>
 					</div>
-					<div className={styles["textarea-group"]}>
-						<textarea placeholder="Message"></textarea>
-					</div>
-
-					<button type="submit">Send</button>
-				</form>
+				</div>
+				<div className={styles["form-container"]}>
+					<form className={styles["contact-form"]}>
+						<div className={styles["form-group"]}>
+							<input type="text" placeholder="Name" />
+							<input type="email" placeholder="Email" />
+						</div>
+						<div className={styles["textarea-group"]}>
+							<textarea placeholder="Message"></textarea>
+						</div>
+						<button type="submit">Submit</button>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
