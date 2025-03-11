4109465c7333d4004aa45a4ba5b2d839be5d13c6
Erik Hansen <erhansen@civiconnect.ca>
2025-03-11 15:54:21 -0400

    framer-motion for projects page animations

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,20 +1,32 @@
 import React from "react";
+import { motion } from "framer-motion";
 import styles from "@/styles/pages/projects.module.css";
 import ProjectCard from "@/components/ProjectCard";
 import { projectData } from "../../util/projectData";
 
-export default function projects() {
+export default function Projects() {
 	return (
 		<div className={styles["projects-page"]}>
 			<h2>My Projects</h2>
 			<div className={styles["projects"]}>
-				{projectData.map((project) => (
-					<ProjectCard
-						className={styles["project-item"]}
-						key={project.id}
-						project={project}
-					/>
-				))}
+				{projectData
+					.sort((a, b) => a.name.localeCompare(b.name))
+					.map((project, index) => (
+						<motion.div
+							className={styles["project-item"]}
+							key={project.id}
+							initial={{ opacity: 0, x: 80 }}
+							whileInView={{ opacity: 1, x: 0 }}
+							viewport={{ once: true }}
+							transition={{
+								duration: 0.8,
+								ease: "easeOut",
+								delay: index * 0.2,
+							}}
+						>
+							<ProjectCard project={project} />
+						</motion.div>
+					))}
 			</div>
 		</div>
 	);
